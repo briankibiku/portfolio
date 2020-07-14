9b2a13cb09bd41fec7e11efccc51a47ae48aebed
b2k <kibiku.kimani@savannahinformatics.com>
2020-07-14 17:01:57 +0300

    Updates

---
+++ precache-manifest.13c65f520c552ce66c46f471772c1c49.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3029e2d1909f51a724bee1f806a53ee5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "72905a94e9a9f68a08f9",
+    "url": "/portfolio/static/css/2.5508ee19.chunk.css"
+  },
+  {
+    "revision": "ad676697fcc7b468859c",
+    "url": "/portfolio/static/css/main.420a13d5.chunk.css"
+  },
+  {
+    "revision": "72905a94e9a9f68a08f9",
+    "url": "/portfolio/static/js/2.125c65ae.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/portfolio/static/js/2.125c65ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad676697fcc7b468859c",
+    "url": "/portfolio/static/js/main.34e139d0.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/portfolio/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/portfolio/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "00e01c62802f3cc92a3412d7e9dfef8d",
+    "url": "/portfolio/static/media/mobile_applications.00e01c62.svg"
+  }
+]);
